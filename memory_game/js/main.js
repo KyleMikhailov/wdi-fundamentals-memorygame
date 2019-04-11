0ed98d57cb7537fbe04939ac7af500c006064364
Kyle Mikhailov <kylemikhailov@Kyles-iMac-2.local>
2019-04-11 11:21:25 -0400

    created  objects array

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,33 @@
 console.log("Up and running!");
-cards = ["queen", "queen", "king", "king"];
+
+
+cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamons",
+		cardImage: "images/queen-of-diamons.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamons",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
+
+
 cardsInPlay = [];
+
+
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		console.log("You found a match!");
@@ -9,9 +36,12 @@ var checkForMatch = function() {
 	}	
 };
 
+
 var flipCard = function (cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	checkForMatch();
 };
 
